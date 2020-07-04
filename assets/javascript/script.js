9cd0ac4b2a608be5414385909ffb8aaad943e5ab
SkylerTrent <trent.skyler56@gmail.com>
2020-07-04 13:18:48 +0000

    new logo photo

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+$("#outline").mouseenter(function(){
+        $(this).removeClass('border');
+        $(this).addClass('outline');
+    });
+    
+    $('h1').hover(function() {
+  $('h1').css("background", "lightblue");
+  
+})
+
